e482fff3517e8c003054c4501c4a8ba0a937840c
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-08 18:19:05 +0700

    fix: db issue
    fix can't connect to db because time buffering

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
+let cached = global.mongoose;
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
     try{
+        if (cached.conn) return cached.conn;
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`Database connected: ${conn.connection.host}`);
+        if(!cached.promise) {
+            cached.promise = mongoose.connect(process.env.MONGODB_URL, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }).then((mongoose) => mongoose);
+        }
+        cached.conn = await cached.promise;
+        console.log(`Database connected: ${cached.conn.connection.host}`);
+        return cached.conn;
     }
     catch (err) {
         console.error(err);
