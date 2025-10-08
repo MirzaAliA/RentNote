2897257e493c74e17291748c838b6748592deec2
MirzaAliA <mirzaaliabhipraya@gmail.com>
2025-10-08 13:43:34 +0700

    feat: automatic email notification
    add notification by email to remind people to return the bike back on time

---
+++ server/config/nodeCron.js
@@ -0,0 +1,7 @@
+import cron from "node-cron";
+
+const nodeCron = () => {
+    cron.schedule('', () => {
+
+    })
+}
---
+++ server/services/emailService.js
@@ -0,0 +1,50 @@
+import nodemailer from "nodemailer";
+import Tenant from "../models/TenantSchema.model.js";
+import { sendErrorResponse, sendOkResponse } from "../core/responses.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GOOGLE_EMAIL,
+        pass: process.env.GOOGLE_APP_PASSWORD
+    },
+});
+
+await transporter.verify();
+
+
+export const sendEmail = async (req, res) => {
+    try {
+        const date = new Date();
+        const hours17 = 1000 * 60 * 60 * 17;
+        const plus17Hours = new Date(date.getTime() + hours17);
+        const tenantDatas = await Tenant.find({ "vehicleInformation.rentalEndDate": { $lt: plus17Hours } });
+
+        await Promise.all(
+            tenantDatas.map(tenantData => {
+                transporter.sendMail({
+                    from: `Save Rental Motor Semarang <saverentalsmg@gmail.com>`,
+                    to: tenantData.email,
+                    subject: "Notifikasi Otomatis: Pengingat habis masa sewa motor kamu yah :)",
+                    text: `Halo ${tenantData.name}, masa sewa motor kamu akan berakhir pada ${tenantData.vehicleInformation.rentalEndDate.toLocaleString("id-ID", {
+                        timeZone: "Asia/Jakarta",
+                        weekday: "long",
+                        day: "numeric",
+                        month: "long", 
+                        year: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit"
+                    })}. Jangan lupa untuk mengembalikannya tepat waktu yah :). \n\n\n\nIni adalah pesan otomatis, jangan balas pesan ini. Jika Anda perlu bantuan, silakan hubungi Admin Save Rental.\n\nwa.me/6281227454993\nsaverentalsmg@gmail.com`
+                })
+            })
+        )
+
+        sendOkResponse(res, tenantDatas.email, "Success");
+    }
+    catch (err) {
+        sendErrorResponse(res, { message: "Error while sending automatic notification through email" }, 500)
+    }
+}
